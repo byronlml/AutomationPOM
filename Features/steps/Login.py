4c40fb026e520f1f56e38089634079005d0f2c10
Byron Orozco <baorozco2@misena.edu.co>
2023-02-27 13:17:00 -0500

    modificacion de nombres de funciones

--- Features/steps/Login.py
+++ Features/steps/Login.py
@@ -9,22 +9,21 @@ url = "https://practicetestautomation.com/practice-test-login/"
 @given('User on login page')
 def open_url(self):
     WebDriverFunctions(self.driver).open_url(url)
-    time.sleep(5)
 
 
 @when('User enter "{username}" and "{password}"')
-def step_impl(self, username, password):
+def user_credentials(self, username, password):
     LoginPage(self.driver).username(username)
     LoginPage(self.driver).password(password)
 
 
 @when('User click on login button')
-def step_impl(self):
+def login_button(self):
     LoginPage(self.driver).login_button()
     time.sleep(1)
 
 
 @then('Would show the user page')
-def step_impl(self):
+def user_page(self):
     text = LoginPage(self.driver).logged_user()
     assert text == "Logged In Successfully"
